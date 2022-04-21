ecd4c8b263461504d7802c2a0f537e5808650881
LuizFJP <luizfjportela@gmail.com>
2022-04-21 11:34:35 -0300

    feat: create client connection

---
+++ src/controller/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/LuizFJP/currency-coin-grpc-go/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to connect %v\n", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewCurrencyCoinServiceClient(conn)
+}
