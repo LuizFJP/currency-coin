96e19c39f3a0a20c47a4c054bf6b591ba60eec4f
LuizFJP <luizfjportela@gmail.com>
2022-04-21 13:15:30 -0300

    feat: initiate variavle collection

--- src/model/server/main.go
+++ src/model/server/main.go
@@ -11,6 +11,7 @@ import (
 	"google.golang.org/grpc"
 )
 
+var collection *mongo.Collection
 var addr string = "0.0.0.0:50051"
 
 type Server struct {
@@ -22,13 +23,13 @@ func main() {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:root@localhost:27017/"))
 
 	if err != nil {
-		log.Fatalf(err)
+		log.Fatal(err)
 	}
 
 	err = client.Connect(context.Background())
 
 	if err != nil {
-		log.Fatalf(err)
+		log.Fatal(err)
 	}
 
 	collection = client.Database("coinsdb").Collection("coins")
