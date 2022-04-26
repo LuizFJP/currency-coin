19e76df810de61d70ff6b2b0d4361d2514c95275
LuizFJP <luizfjportela@gmail.com>
2022-04-26 01:49:40 -0300

    feat: refactor-down-code

--- src/server/downvote_coin.go
+++ src/server/downvote_coin.go
@@ -6,37 +6,16 @@ import (
 
 	pb "LuizFJP/currency-coin-grpc-go/proto"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+
 )
 
 func (s *Server) DownvoteCoin(ctx context.Context, in *pb.CoinRequest) (*pb.CoinResponse, error) {
-	result := &CoinItem{}
-	data := &CoinItem {
-		Name: in.Name,
-	}
-
-	err := data.UpdateValidate()
+	result, err := Vote(in, -1)
 	if err != nil {
 		return nil, fmt.Errorf(
 			codes.InvalidArgument.String(),
 			fmt.Sprint(err),
-		)
-	}
-	err = UpdateByName(data.Name, -1)
-
-	if err != nil {
-		return nil, err
-	}
-	
-	err = collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "name", Value: data.Name}}).Decode(&result)
-	
-	if err != nil {
-		return nil, status.Errorf(
-			codes.Internal,
-			fmt.Sprintf("Internal Error: %v", err),
 		)
 	}
 
